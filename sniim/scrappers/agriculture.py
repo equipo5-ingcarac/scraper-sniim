f0afb030ee365f2008a981780fa047c45dd2ea10
jmartingv <jmartingv96@gmail.com>
2023-09-24 22:13:26 -0700

    inital modificactions

--- sniim/scrappers/agriculture.py
+++ sniim/scrappers/agriculture.py
@@ -1,13 +1,26 @@
 import datetime
+import time
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
-from sniim.db.mongo import Mongoclient
 from clint.textui import puts, colored, indent
 
+def normalize(s):
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+    )
+    for a, b in replacements:
+        s = s.replace(a, b).replace(a.upper(), b.upper()).replace(' ','_').lower().replace('/','')
+    return s
 
 class ScrapperMarketAgriculture:
     total_records = 0
     inserted_records = 0
+    
 
     base_url = 'http://www.economia-sniim.gob.mx/NUEVO/Consultas/MercadosNacionales/PreciosDeMercado/Agricolas'
     init_urls = [
@@ -18,8 +31,7 @@ class ScrapperMarketAgriculture:
     ]
 
     def __init__(self, *args, **kwargs):
-        self.is_historic = kwargs.get('is_historic', True)
-        self.mongo = Mongoclient(db_collection='agricultura')
+        self.is_historic = kwargs.get('is_historic', False)
 
     def read_category(self, category, url, url_form):
         category_page = requests.get(self.base_url + url)
@@ -36,7 +48,7 @@ class ScrapperMarketAgriculture:
                 puts(colored.magenta("Producto: {}".format(str(product_name))))
 
             if self.is_historic:
-                for year in range(1999, 2019):
+                for year in range(1999, 2023):
                     payload = {
                         'fechaInicio':'01/01/{0}'.format(str(year)),
                         'fechaFinal':'01/01/{0}'.format(str(year + 1)),
@@ -49,7 +61,7 @@ class ScrapperMarketAgriculture:
                         'RegistrosPorPagina':'1000'
                     }
 
-                    if not self.gather_prices(payload, url_form):
+                    if not self.gather_prices(payload, url_form, product_name):
                         next
             else:
                 today = datetime.datetime.today()
@@ -66,7 +78,7 @@ class ScrapperMarketAgriculture:
                         'RegistrosPorPagina':'1000'
                     }
 
-                if not self.gather_prices(payload, url_form):
+                if not self.gather_prices(payload, url_form, product_name):
                     continue
 
         return
@@ -77,8 +89,10 @@ class ScrapperMarketAgriculture:
 
         for category, url, url_form in self.init_urls:
             self.read_category(category, url, url_form)
+            time.sleep(30)
 
-    def gather_prices(self, payload, url_form):
+    def gather_prices(self, payload, url_form, product_name):
+
         with indent(4):
             puts(colored.blue("Peticion: {}".format(str(payload))))
 
@@ -103,6 +117,8 @@ class ScrapperMarketAgriculture:
         fields = ('fecha', 'presentacion', 'origen', 'destino', 'precio_min', 'precio_max', 'precio_frec', 'obs')
         counter_row = 0
 
+        df = pd.DataFrame(columns=fields)
+
         # print(table_prices)
         for observation in table_prices.find_all('tr'):
             if counter_row > 1:
@@ -115,24 +131,28 @@ class ScrapperMarketAgriculture:
 
                 with indent(4):
                     puts(colored.yellow("Insertando: {}".format(str(row))))
+                
+                df = df.append(row,ignore_index=True)
 
-                if self.mongo.insert_one(row):
-                    self.inserted_records += 1
-                    with indent(4):
-                        puts(colored.green("Insertado: {}".format(str(row))))
-                else:
-                    with indent(4):
-                        puts(colored.red("No Insertado: {}".format(str(row))))
+                # if self.mongo.insert_one(row):
+                #     self.inserted_records += 1
+                #     with indent(4):
+                #         puts(colored.green("Insertado: {}".format(str(row))))
+                # else:
+                #     with indent(4):
+                #         puts(colored.red("No Insertado: {}".format(str(row))))
 
             self.total_records += 1
             counter_row += 1
-
+            
+        
+        df.to_csv(f"./data/{normalize(product_name)}.csv")
         return True
 
 
-# if __name__ == '__main__':
-#     # agricola = ScrapperMarketAgriculture()
-#     # agricola.scraping()
+if __name__ == '__main__':
+    agricola = ScrapperMarketAgriculture()
+    agricola.scraping()
 
 #     vacas = ScrapperMarketLiveStock()
 #     vacas.scraping()
